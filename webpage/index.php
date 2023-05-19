e56b11d2440d27ae1556cce8895ceef0151a7e25
Beniamin <beniodolega@gmail.com>
2023-05-19 15:52:23 +0200

    Added form.html

--- webpage/index.php
+++ webpage/index.php
@@ -1,7 +1,23 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
-$country = geoip_country_code3_by_name($ip);
+$country = geoip_country_name_by_name($ip);
 
-echo 'Twoje IP: '.$ip.'<br>';
-echo 'Twój kraj: '.$country;
+if($_POST["ipadress"]==null){
+    
+    if($country==null){
+        echo '<meta http-equiv="refresh" content="0;url=form.html">';
+        exit;
+    }
+    else{
+        echo 'Twoje IP: '.$ip.'<br>';
+        echo 'Twój kraj: '.$country;
+    }
+}
+else{
+    $ip = $_POST["ipadress"];
+    $country = geoip_country_name_by_name($ip);
+        
+    echo 'Twoje IP: '.$ip.'<br>';
+    echo 'Twój kraj: '.$country;    
+}
 ?>
