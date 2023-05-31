96aeb7b0899798822ac7c7a2a8e5a59d657c6a60
Beniamin <beniodolega@gmail.com>
2023-05-31 13:46:11 +0200

    Added database query insertion to index.php and removed uneccecary elements from .gitignore

--- webpage/index.php
+++ webpage/index.php
@@ -13,30 +13,42 @@ try {
     die('Błąd połączenia z bazą danych: ' . $e->getMessage());
 }
 
-/*
 //query
-if($_POST["ipadress"]!=null){
-    $ip = $_POST["ipadress"];
-}
-else{
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $country = geoip_country_name_by_name($ip);
-}
+$ip = $_SERVER['REMOTE_ADDR'];
+$country = geoip_country_name_by_name($ip);
+$date = date("Y-m-d H:i:s");
 
 if($_POST["ipadress"]==null && $country==null){    
     echo '<meta http-equiv="refresh" content="0;url=form.html">';
     exit;
 }
 else{
-    $country = geoip_country_name_by_name($ip);
+    //user feedback
+    $request_ip = $_POST["ipadress"];
         
     echo 'Twoje IP: '.$ip.'<br>';
 
-    if($country!=null){
+    if($country==null){
+        $country = geoip_country_name_by_name($request_ip);
+        
+        if($country!=null){
+            echo 'Twój kraj: '.$country;
+        }
+        else{
+            echo 'Twój kraj: Skrypt nie jest w stanie określić twojego kraju.';
+        }
+
+        //database insertion
+        $sql = 'INSERT INTO ip_query_data(Date, User_ip, Request_ip, Country) VALUES(:date, :ip, :request_ip, :country)';
+        $statement = $db->prepare($sql);
+        $statement->execute(array(
+            ':date' => $date,
+            ':ip' => $ip,
+            ':request_ip' => $request_ip,
+            ':country' => $country
+        ));
+    }
+    else{     
         echo 'Twój kraj: '.$country;
-    }
-    else{
-        echo 'Twój kraj: Skrypt nie jest w stanie określić twojego kraju.';
-    }
+    }   
 }
-*/
